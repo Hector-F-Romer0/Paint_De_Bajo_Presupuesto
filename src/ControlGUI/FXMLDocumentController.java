7e188b18a467bac6547b4c62a3b0c98624b19bb3
Hector-F-Romer0 <hector_fabio.romero@uao.edu.co>
2022-03-20 11:51:05 -0500

    Se creó la cruz

--- src/ControlGUI/FXMLDocumentController.java
+++ src/ControlGUI/FXMLDocumentController.java
@@ -38,7 +38,9 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Canvas lienzo;
-    GraphicsContext g;
+    
+    @FXML
+    private GraphicsContext g;
         
     @FXML
     private Pane panelFiguras;
@@ -53,17 +55,12 @@ public class FXMLDocumentController implements Initializable {
     private ImageView sun_jinwoo;
     
     
-    
     @FXML
-    private void crearHexagono(MouseEvent event) {
-        
-        coorX = event.getX();
-        coorY = event.getY();   
+    private void crearHexagono(ActionEvent event) {
         System.out.println("El punto " + coorX + ", " + coorY);
-        
-        
+
         listaPuntos = new LinkedList<>();
-        int r = 100;
+        int r = 40;
         g.setStroke(colorRelleno.getValue());
         int numLados = 6;
         double angulo = (2 * Math.PI)/ numLados;
@@ -86,29 +83,91 @@ public class FXMLDocumentController implements Initializable {
             System.out.println("Coordenada: " + i + "x: " + x1[i] + "y: " + y1[i]);
         }
         
-        Punto2D puntito = new Punto2D(x1[0], y1[0]);
-        listaPuntos.add(puntito);
+        Punto2D puntoInicial = new Punto2D(x1[0], y1[0]);
+        listaPuntos.add(puntoInicial);
         for (int i = 1; i < x1.length; i++) {
             Punto2D punto = new Punto2D(x1[i], y1[i]);
             listaPuntos.addLast(punto);
         }
+        // Se crea el polígono
+        g.strokePolygon(x1, y1, numLados);
+        g.setLineWidth(3);
         
         //HOLA
         
     }
     
-    @FXML
-    private void crearHeptagono(MouseEvent event) {
-        
-        coorX = event.getX();
-        coorY = event.getY();   
-        System.out.println("El punto " + coorX + ", " + coorY);
-    }
+//    @FXML
+//    private void crearHeptagono(MouseEvent event) {
+//        
+//        coorX = event.getX();
+//        coorY = event.getY();   
+//        System.out.println("El punto " + coorX + ", " + coorY);
+//    }
    
+    @FXML
+    private void crearCruz(ActionEvent event) {
+        listaPuntos = new LinkedList<>();
+        int r = 70;
+        g.setStroke(colorRelleno.getValue());
+        double v = (0.2*r);
+        double w = (0.78*r);
+        
+        // Obtenemos las coordenadas del centro de la circunferencia donde incribiremos la cruz
+        double h = coorX;
+        double k = coorY;
+        x1 = new double[12];
+        y1 = new double[12];
+        
+        x1[0] = v+h;
+        y1[0] = v+k;
+        
+        x1[1] = v+w+h;
+        y1[1] = v+k;
+        
+        x1[2] = v+w+h;
+        y1[2] = -v+k;
+        
+        x1[3] = v+h;
+        y1[3] = -v+k;
+        
+        x1[4] = v+h;
+        y1[4] = -v-w+k;
+        
+        x1[5] = -v+h;
+        y1[5] = -v-w+k;
+        
+        x1[6] = -v+h;
+        y1[6] = -v+k;
+        
+        x1[7] = -v-w+h;
+        y1[7] = -v+k;
+        
+        x1[8] = -v-w+h;
+        y1[8] = v+k;
+        
+        x1[9] = -v+h;
+        y1[9] = v+k;
+        
+        x1[10] = -v+h;
+        y1[10] = v+w+k;
+        
+        x1[11] = v+h;
+        y1[11] = v+w+k;
+        
+        for (int i = 0; i < x1.length; i++) {
+            Punto2D punto = new Punto2D(x1[i], y1[i]);
+            listaPuntos.addLast(punto);
+            System.out.println("Coordenada: " + i + "x: " + x1[i] + "y: " + y1[i]);
+        }
+        
+        // Se crea el polígono
+        g.strokePolygon(x1, y1,12);
+        g.setLineWidth(3);
+    }
     
     @FXML
-    private void obtenerCoordenadas(MouseEvent event) {
-        
+    private void obtenerCoordenadas(MouseEvent event) { 
         coorX = event.getX();
         coorY = event.getY();   
         System.out.println("El punto " + coorX + ", " + coorY);
